611b99e5a00223fb6db529f3efdc2f9d8f6093ac
iGWE-eDWIN <igweedwin15@gmail.com>
2025-07-29 06:03:18 +0100

    second commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://nexusserver-laum.onrender.com';
 
 // Create axios instance
 const api = axios.create({
